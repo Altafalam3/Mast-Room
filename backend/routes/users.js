0f92f98619a5c2d17dcf95c446f116ff720e8bd0
Altafalam3 <altaf.alam0032@gmail.com>
2023-08-08 22:19:43 +0530

    done

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,19 +4,6 @@ import { verifyAdmin, verifyToken,verifyUser } from "../utils/verifyToken.js";
 
  const router=express.Router();
 
- /*router.get("/checkauthentication",verifyToken,(req,res,next)=>{
-    res.send("hello user ,You are logged in..")
- })
-
- router.get("/checkuser/:id",verifyUser,(req,res,next)=>{
-   res.send("hello user ,You are logged in and you can delete your account..")
-})
- 
-
-router.get("/checkadmin/:id",verifyAdmin,(req,res,next)=>{
-   res.send("hello admin ,You are logged in and you can delete all accounts..")
-})*/
-
  //update
  router.put("/update/:id",verifyUser,updateUser);
 
